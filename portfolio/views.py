a73024d339528d3e3c7df5648ee1564ef3f82e63
ajanet1993 <ajanet@unomaha.edu>
2017-09-17 23:41:38 -0500

    add new changes

--- portfolio/views.py
+++ portfolio/views.py
@@ -107,7 +107,7 @@ def investment_new(request):
            investment = form.save(commit=False)
            investment.created_date = timezone.now()
            investment.save()
-           investments = Investment.objects.filter(purchase_date__lte=timezone.now())
+           investments = Investment.objects.filter(acquired_date__lte=timezone.now())
            return render(request, 'portfolio/investment_list.html',
                          {'investments': investments})
    else:
